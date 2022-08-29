5e531fa32f5a7cfce6acb2656d83139b9e310095
Nasir Hafeez <nasirhafeez2002@gmail.com>
2022-08-29 17:31:41 +0500

    initial commit

---
+++ jp69ndw2/api.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_GET['venue-info-url'])) {
+  header( "Content-type: application/json" );
+
+  $jsonData = [
+    "captive" => true,
+    "user-portal-url" => "https://capport.nasirhafeez.com/guest/s/jp69ndw2",
+    "venue-info-url" => "https://capport.nasirhafeez.com/guest/s/jp69ndw2",
+    "seconds-remaining" => 63200,
+    "can-extend-session" => true
+  ];
+
+  echo json_encode($jsonData, JSON_PRETTY_PRINT);
+}
